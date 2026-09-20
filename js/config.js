1cb7354369b59e7d665f3530b029ae4e9412b3fd
Pooran-Raikwar <pooranraikwar999@gmail.com>
2026-09-20 22:22:09 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+/* ONE-TIME SETUP: paste your deployed Google Apps Script /exec URL here. */
+const DEFAULT_API_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+const SCHOOL_CONFIG = {
+  name: "Govt. Sandipani HSS School Damoh",
+  subtitle: "Vocational Education • IT–ITeS • Digital Campus",
+  classes: ["9th","10th","11th","12th"],
+  vocationalTrade: "IT-ITeS",
+  vocationalJobRole: "Domestic Data Entry Operator"
+};
