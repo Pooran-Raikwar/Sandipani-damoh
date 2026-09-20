6022065412525f51b21f5d7f9f538a007ef607e9
Pooran-Raikwar <pooranraikwar999@gmail.com>
2026-09-20 22:55:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* ONE-TIME SETUP: paste your deployed Google Apps Script /exec URL here. */
-const DEFAULT_API_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbybBChgXswtDB444meBURz9S-tOydp-MmkA5YQw1_vs8Rr0haOwpPwa0ac_w8ALu7n__Q/exec";
 const SCHOOL_CONFIG = {
   name: "Govt. Sandipani HSS School Damoh",
   subtitle: "Vocational Education • IT–ITeS • Digital Campus",
