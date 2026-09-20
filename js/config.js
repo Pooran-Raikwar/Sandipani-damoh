80a22cf8ce806937604eee184c9b01b20d36922a
Pooran-Raikwar <pooranraikwar999@gmail.com>
2026-09-21 01:19:40 +0530

    Delete js/config.js

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Sandipani GitHub Pages configuration
-// Keep the Apps Script deployment URL here. Do NOT put ADMIN_PIN in this file.
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbybBChgXswtDB444meBURz9S-tOydp-MmkA5YQw1_vs8Rr0haOwpPwa0ac_w8ALu7n__Q/exec"
-};
